920d6c573ba35af917668d9f6f81847fe7659cd5
alaa-7amdi-dev <hamdihafezalaa@gmail.com>
2026-02-27 14:18:11 +0200

    Part 01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 
+            //A:Composition (Has_A Strong)=>  is a HAS-A relationship where the child can not exists Without parent.
+            //B:Association (Use_A)=> connection between two classes ,one class use the other ,but no ownership betwwen them.
+            //C:Inheritance (Is_A)=>one class (child) inherits from another (parent).
+            //D:Aggregation (Has_A Weak)=> ia a Has-A relationship where the child can exists indpendentiy of the Parent.
+            //E:Dependency => class temporarily depends on another to perform an action.
+
+            #endregion
+
         }
     }
 }
